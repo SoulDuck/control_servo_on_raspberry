5822e780b70b44904525a1185472c4f2a3ac1ab0
kimseongjung <plznw4me@naver.com>
2018-09-28 09:30:57 +0900

    servo on Rasberry pi

---
+++ multiple_servo.py
@@ -0,0 +1,35 @@
+from time import sleep
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pan = 27
+tilt = 17
+z_tilt = 22
+
+GPIO.setup(tilt, GPIO.OUT) # white => TILT
+GPIO.setup(pan, GPIO.OUT) # gray ==> PAN
+GPIO.setup(z_tilt, GPIO.OUT) # gray ==> PAN
+
+def setServoAngle(servo, angle):
+    assert angle >=30 and angle <= 150 , ''
+    for i in range(5,angle):
+        pwm = GPIO.PWM(servo, 100)
+        pwm.start(20)
+        dutyCycle = angle / 18. + 3.
+        pwm.ChangeDutyCycle(dutyCycle)
+        sleep(0.1)
+        pwm.stop()
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        setServoAngle(pan, 150)
+        setServoAngle(tilt, 150)
+    else:
+
+        print sys.argv
+        setServoAngle(pan,int(sys.argv[1])) # 30 ==> 90 (middle point) ==> 150
+        setServoAngle(tilt, int(sys.argv[2])) # 30 ==> 90 (middle point) ==> 150
+        #setServoAngle(z_tilt, int(sys.argv[2]))  # 30 ==> 90 (middle point) ==> 150
+    GPIO.cleanup()
--- servo.py
+++ servo.py
@@ -1,11 +1,25 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
-servoPin = 11
-GPIO.setup(servoPin ,GPIO.OUT)
-pmw = GPIO.PWM(servoPin , 50 )
-pwm.start
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--angle' , type=int)
+args = parser.parse_args()
+
+
+
+
+
+#this sets the names to board mode,
+## which just names the pins according to the numbers
+# #in the middle of the diagram above.
+GPIO.setup(03, GPIO.OUT)
+#setup PWM on pin #3 at 50Hz
+pwm=GPIO.PWM(03, 50)
+pwm.start(0)
+# Then start it with 0 duty cycle so
+# it doesn't set any angles on startup
 
 def SetAngle(angle):
     duty = angle / 18 + 2
@@ -18,6 +32,8 @@ def SetAngle(angle):
 
 
 if __name__ == '__main__':
-    SetAngle(90)
+    print 'Angle : {}'.format(args.angle)
+    SetAngle(args.angle)
+    sleep(3)
     pwm.stop()
     GPIO.cleanup()
